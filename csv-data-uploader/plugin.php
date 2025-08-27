bc4409a740f55e26bab0e8d6480e2d6a3cd8fe38
rajvindergithub <rajvindergithub@gmail.com>
2025-08-27 20:56:18 +0530

    update code

--- csv-data-uploader/plugin.php
+++ csv-data-uploader/plugin.php
@@ -1,8 +1,8 @@
 <?php 
 /*
  * Plugin Name:       CSV Data Uploader
- * Description:       Handle the basics with this plugin.
- * Author:            John Smith
+ * Description:       Upload your CSV file into WordPress Table
+ * Author:            Rajvinder Singh
  * Text Domain:       csv-data-uploader
  */
 
@@ -11,12 +11,55 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Stop execution
 }
 
+define("CDU_PLUGIN_DIR_PATH", plugin_dir_path(__FILE__));
 
 add_shortcode("csv-data-uploader", "cdu_display_uploader_form");
 
+add_action("wp_enqueue_scripts","cdu_add_script_file");
+
+function cdu_add_script_file(){
+    
+    wp_enqueue_script("cdu-script-js", CDU_PLUGIN_DIR_PATH.'/assets/js/script.js');
+    
+}
+
 function cdu_display_uploader_form(){
     
-    return "<h3>CSV Data Uploader Fetch Result</h3>";
+    ob_start();
+    
+        include(CDU_PLUGIN_DIR_PATH. '/template/cdu_form.php');
+    
+        $template = ob_get_contents(); 
+    
+        ob_end_clean(); 
+    
+    return $template;
+    
+}
+
+register_activation_hook( __FILE__, 'create_students_table' );
+
+function create_students_table() {
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . 'students_data';
+	$charset_collate = $wpdb->get_charset_collate();
+
+	// Load dbDelta
+	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+	$sql = "CREATE TABLE IF NOT EXISTS {$table_name} (
+		id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+		name VARCHAR(100) NOT NULL,
+		email VARCHAR(191) NOT NULL,
+		age SMALLINT(3) UNSIGNED NULL,
+		phone VARCHAR(32) NULL,
+		photo VARCHAR(255) NULL,
+		PRIMARY KEY  (id),
+		UNIQUE KEY email (email)
+	) {$charset_collate};";
+
+	dbDelta( $sql );
     
 }
 
---
+++ csv-data-uploader/template/cdu_form.php
@@ -0,0 +1,17 @@
+<h2>
+    Form Data
+</h2>
+
+<style type="text/css">
+    .fields_form label{ width: 100%; display: block; font-weight: 600; font-size: 1.25rem; margin-bottom: 1rem; color: brown; }
+</style>
+
+<form action="">
+    <p class="fields_form">
+        <label for="csv_data_file">Upload CSV File</label>
+        <input type="file" name="csv_data_file" id="csv_data_file" />
+    </p>
+    <p class="fields_form">
+        <button type="submit">Upload CSV</button>
+    </p>
+</form>
