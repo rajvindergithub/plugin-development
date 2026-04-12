7ca3007f1f03834a099f137729a81b65fc53a705
rajvindergithub <rajvindergithub@gmail.com>
2026-04-12 22:27:27 +0530

    Form User Data

---
+++ form-user-data-country-state-city/plugin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: User Custom Form Data 
+Description: Store Custom Form Data Use with Shortcode [custom-user-form]
+Version: 1.0
+Author: Rajvinder Singh
+*/
+
+if (!defined('ABSPATH')) exit;
+
+function create_custom_user_form_table{
+    
+    global $wpdb
+    
+}
